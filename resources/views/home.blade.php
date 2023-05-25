984dc751502ae11664ecab878e8538fdf7c18325
Akbar0912 <ap411342@gmail.com>
2023-05-25 10:30:12 +0700

    update: laravel fitur

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,29 +7,26 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Web Gis</title>
 
-    <!-- Leaflet Routing Machine Library -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet-routing-machine/3.2.12/leaflet-routing-machine.css"
-        integrity="sha512-82egxSKTIoB6EXIjllwz+Xf6etSIVZ13wKJBGImtSxEKjGpsXZlax2kLgtrUqYV61FcfsY8Pds6e/1HjmSsBGw=="
-        crossorigin="" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-routing-machine/3.2.12/leaflet-routing-machine.js"
-        integrity="sha512-7c2oTSXf6uIA7Ud6pZehi9+5QZI/CiAAetbNh4qHkUc7AT2OLutPi4T1q/Uj5+u5ChwsK1Me67wL9G1G5dpAOA=="
-        crossorigin=""></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
 
-    <!-- Stylesheet Leaflet -->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+        integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
 
-    <!-- Leaflet Library -->
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-
-    <!-- jQuery Library -->
+    <script src="https://unpkg.com/esri-leaflet-vector@4.0.1/dist/esri-leaflet-vector.js" crossorigin=""></script>
     <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
         crossorigin="anonymous"></script>
 
-    <!-- Esri Leaflet Vector Library -->
-    <script src="https://unpkg.com/esri-leaflet-vector@4.0.1/dist/esri-leaflet-vector.js" crossorigin=""></script>
 
+    <script src="geojson/tes.geojson"></script>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+    <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <style>
         #map {
             height: 600px;
@@ -62,8 +59,8 @@
 
         // Icon untuk marker Bus
         var busIcon = L.icon({
-            iconUrl: 'assets/icons/pin.png',
-            iconSize: [50, 55],
+            iconUrl: 'assets/icons/religi-01.png',
+            iconSize: [40, 45],
             shadowSize: [50, 64],
             iconAnchor: [22, 94],
             shadowAnchor: [4, 62],
@@ -98,8 +95,13 @@
                         "<br><strong>Alamat:</strong> " + properties.Alamat +
                         "<br><strong>Latitude:</strong> " + latitude +
                         "<br><strong>Longitude:</strong> " + longitude +
-                        "<br><button onclick='return keSini(" + latitude + longitude +
-                        ")'>Button</button>";
+                        "<br><button class='btn btn-info' onclick='return keAwal(" + latitude + ", " +
+                        longitude + ")'>Start</button>" +
+                        " ||| <button class='btn btn-success' onclick='return keAkhir(" + latitude + ", " +
+                        longitude + ")'>Dest</button>" +
+                        " ||| <button class='btn btn-danger' onclick='return stopRouting(" + latitude +
+                        ", " + longitude + ")'>Remove Route</button>";
+
 
                     // Mengikat konten popup ke layer
                     layer.bindPopup(popupContent);
@@ -107,21 +109,52 @@
             }).addTo(map);
         });
 
+
+        // L.Routing.control({
+        //     waypoints: [
+        //         L.latLng(-7.599187514, 112.78720839799996),
+        //         L.latLng(-7.38659453000002, 112.733095929)
+        //     ]
+        // }).addTo(map);
+
         // Membuat rute antara tempat wisata religi A dan B
-        L.Routing.control({
-            waypoints: [
-                L.latLng(-7.5982513440231685, 112.23599844772906),
-                L.latLng(-7.561940028535261, 112.34746263818818)
-            ],
-            show: true, // Menampilkan garis jarak
-            lineOptions: {
-                styles: [{
-                    color: 'blue',
-                    opacity: 5,
-                    weight: 10
-                }]
+        var control = null;
+
+        function Routing() {
+            control = L.Routing.control({
+                waypoints: [
+                    L.latLng(),
+                    L.latLng()
+                ],
+                createMarker: function() {
+                    // Mengembalikan null untuk menghindari pembuatan ikon default
+                    return null;
+                }
+            })
+            control.addTo(map);
+        }
+
+
+        function keAwal(latitude, longitude) {
+            var latLng0 = L.latLng(latitude, longitude);
+            if (control == null) {
+                Routing();
+                control.spliceWaypoints(0, 1, latLng0);
+                control.spliceWaypoints(control.getWaypoints().length - 1, 1, latLng1);
             }
-        }).addTo(map);
+            control.spliceWaypoints(0, 1, latLng0);
+
+        }
+
+        function keAkhir(latitude, longitude) {
+            var latLng1 = L.latLng(latitude, longitude);
+            control.spliceWaypoints(control.getWaypoints().length - 1, 1, latLng1);
+        }
+
+        function stopRouting() {
+            control.remove();
+            control = null;
+        }
     </script>
 </body>
 
