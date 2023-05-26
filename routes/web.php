8ee1d914c397c7483ca14d6324bc5948aa8d5318
bruhtwig <emhaaji.putrazaman@gmail.com>
2023-05-26 17:49:29 +0700

    landing page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/data', function () {
+    return view('data');
 });
